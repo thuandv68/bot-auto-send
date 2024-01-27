93814c29a7b67444fc8b9d9f4b3782531bed6754
thuandv68 <94217873+thuandv68@users.noreply.github.com>
2024-01-27 09:46:59 +0700

    Init project

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    TOKEN: "",
+    PRIVATE_KEY: "",
+    ADDR_TO: "",
+    DELAY: 1, // s
+    RPC: {
+        URL: "https://linea.drpc.org",
+        NAME: 'Linea Mainnet',
+        CHAIN_ID: 59144
+    }
+}	
