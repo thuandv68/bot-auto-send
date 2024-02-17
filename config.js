28fc68b4f2e7ece8b9560801504bc26e5c100b05
thuandv68 <94217873+thuandv68@users.noreply.github.com>
2024-02-17 10:04:03 +0700

    Update Input password

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
     TOKEN: "0x176211869cA2b568f2A7D4EE941E073a821EE1ff", // USDC
-    PRIVATE_KEY: "",
-    ADDR_TO: "",
+    ADDR_TO: "0xe8a09f24df64ce8714778a8c54f0259bb041236d",
     DELAY: 1, // s
     RPC: {
         URL: "https://1rpc.io/linea",
