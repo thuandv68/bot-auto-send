e0bee5175aff7d70be7aad3e1e6681140d983930
thuandv68 <94217873+thuandv68@users.noreply.github.com>
2024-02-17 12:32:18 +0700

    Update bot vs PM2

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     TOKEN: "0x176211869cA2b568f2A7D4EE941E073a821EE1ff", // USDC
     ADDR_TO: "0xe8a09f24df64ce8714778a8c54f0259bb041236d",
+    PRIVATE_KEY: "",
     DELAY: 1, // s
     RPC: {
         URL: "https://1rpc.io/linea",
